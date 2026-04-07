938d3f19eb5dc2f39b1f8f242fd4161b76c9ecf2
Dariya <your_email@example.com>
2026-04-07 22:28:20 +0000

    /

---
+++ exampleED2_build/CMakeFiles/exampleED.dir/src/ActionInitialization.cc.o.d
@@ -0,0 +1,9 @@
+CMakeFiles/exampleED.dir/src/ActionInitialization.cc.o: \
+ /home/Dariya/exampleED2/src/ActionInitialization.cc \
+ /home/Dariya/miniconda3/x86_64-conda-linux-gnu/sysroot/usr/include/stdc-predef.h \
+ /home/Dariya/exampleED2/include/ActionInitialization.hh \
+ /home/Dariya/geant4-v11.3.2/install/include/Geant4/G4VUserActionInitialization.hh \
+ /home/Dariya/exampleED2/include/PrimaryGeneratorAction.hh \
+ /home/Dariya/geant4-v11.3.2/install/include/Geant4/G4VUserPrimaryGeneratorAction.hh \
+ /home/Dariya/exampleED2/include/EventAction.hh \
+ /home/Dariya/geant4-v11.3.2/install/include/Geant4/G4UserEventAction.hh
