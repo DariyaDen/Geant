af0661d031d8ca1a8968001ba40a434e2746c0f9
Dariya <your_email@example.com>
2026-04-07 22:19:23 +0000

    //

---
+++ exampleED2wsol_build/CMakeFiles/exampleED.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/Dariya/exampleED2wsol/exampleED.cc" "CMakeFiles/exampleED.dir/exampleED.cc.o" "gcc" "CMakeFiles/exampleED.dir/exampleED.cc.o.d"
+  "/home/Dariya/exampleED2wsol/src/ActionInitialization.cc" "CMakeFiles/exampleED.dir/src/ActionInitialization.cc.o" "gcc" "CMakeFiles/exampleED.dir/src/ActionInitialization.cc.o.d"
+  "/home/Dariya/exampleED2wsol/src/DetectorConstruction.cc" "CMakeFiles/exampleED.dir/src/DetectorConstruction.cc.o" "gcc" "CMakeFiles/exampleED.dir/src/DetectorConstruction.cc.o.d"
+  "/home/Dariya/exampleED2wsol/src/EventAction.cc" "CMakeFiles/exampleED.dir/src/EventAction.cc.o" "gcc" "CMakeFiles/exampleED.dir/src/EventAction.cc.o.d"
+  "/home/Dariya/exampleED2wsol/src/PrimaryGeneratorAction.cc" "CMakeFiles/exampleED.dir/src/PrimaryGeneratorAction.cc.o" "gcc" "CMakeFiles/exampleED.dir/src/PrimaryGeneratorAction.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
