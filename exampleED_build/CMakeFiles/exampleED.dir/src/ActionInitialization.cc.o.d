3a1f44b5df7df5917064483f7139b91f2a262a98
Dariya <your_email@example.com>
2026-04-07 22:54:35 +0000

    ///

---
+++ exampleED_build/CMakeFiles/exampleED.dir/src/ActionInitialization.cc.o.d
@@ -0,0 +1,9 @@
+CMakeFiles/exampleED.dir/src/ActionInitialization.cc.o: \
+ /home/Dariya/exampleED/src/ActionInitialization.cc \
+ /home/Dariya/miniconda3/x86_64-conda-linux-gnu/sysroot/usr/include/stdc-predef.h \
+ /home/Dariya/exampleED/include/ActionInitialization.hh \
+ /home/Dariya/geant4-v11.3.2/install/include/Geant4/G4VUserActionInitialization.hh \
+ /home/Dariya/exampleED/include/PrimaryGeneratorAction.hh \
+ /home/Dariya/geant4-v11.3.2/install/include/Geant4/G4VUserPrimaryGeneratorAction.hh \
+ /home/Dariya/exampleED/include/EventAction.hh \
+ /home/Dariya/geant4-v11.3.2/install/include/Geant4/G4UserEventAction.hh
---
+++ exampleED_build3/CMakeFiles/exampleED.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exampleED.dir/exampleED.cc.o"
+  "CMakeFiles/exampleED.dir/exampleED.cc.o.d"
+  "CMakeFiles/exampleED.dir/src/ActionInitialization.cc.o"
+  "CMakeFiles/exampleED.dir/src/ActionInitialization.cc.o.d"
+  "CMakeFiles/exampleED.dir/src/DetectorConstruction.cc.o"
+  "CMakeFiles/exampleED.dir/src/DetectorConstruction.cc.o.d"
+  "CMakeFiles/exampleED.dir/src/EventAction.cc.o"
+  "CMakeFiles/exampleED.dir/src/EventAction.cc.o.d"
+  "CMakeFiles/exampleED.dir/src/PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/exampleED.dir/src/PrimaryGeneratorAction.cc.o.d"
+  "exampleED"
+  "exampleED.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/exampleED.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
